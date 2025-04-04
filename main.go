6d9a4a30e917a06a21555369749d32a85dbfaee9
Dekr0 <dekr0.dk@protonmail.com>
2025-04-04 09:14:58 -0600

    Integrate config file; Add new flag to invoke kitty tab fuzzy searh

--- main.go
+++ main.go
@@ -7,11 +7,18 @@ import (
 	"os"
 
 	"github.com/Dekr0/shutil/cd"
+	"github.com/Dekr0/shutil/config"
+	"github.com/Dekr0/shutil/kitty"
 	"github.com/Dekr0/shutil/pkg"
 	"github.com/Dekr0/shutil/rename"
 )
 
 func main() {
+	conf, err := config.LoadConfig()
+	if err != nil {
+		fmt.Printf("Failed to load configuration: %s", err.Error())
+	}
+
     useWalker := flag.Bool(
         "walker",
         false,
@@ -35,6 +42,13 @@ func main() {
         "descendant) with underscore",
     )
 
+	useKittyFzfTab := flag.Bool(
+		"kitty-fzf-tab",
+		false,
+		"Using FzF to search for the tabs in the current active window you want " +
+		"to swap",
+	)
+
 	usePkgAdd := flag.String("pkg-add", "", "Package to be added from the profile")
 	usePkgRm := flag.String("pkg-rm", "", "Package to be removed from the profile")
 	usePkgCategory := flag.String("pkg-category", "other", "Package category")
@@ -47,8 +61,16 @@ func main() {
     }))
 
     if *useWalker {
+		roots := flag.Args()
+		if len(roots) <= 0 {
+			/* Look for bookmarks */
+			if conf != nil {
+				roots = conf.BookmarkRoots
+			}
+		}
+
         out, err := cd.SearchDir(
-            flag.Args(),
+            roots,
             uint8(*useWalkerDepth), uint8(*useWalkerWorker), logger,
         )
         if err != nil {
@@ -73,6 +95,7 @@ func main() {
     }
 	
 	if len(*usePkgAdd) > 0 {
+		/* TODO: store this into .shutil */
 		err := pkg.AddPkg(*usePkgAdd, *usePkgCategory)
 		if err != nil {
 			fmt.Println(err.Error())
@@ -83,6 +106,15 @@ func main() {
 
 	if len(*usePkgRm) > 0 {
 		err := pkg.RmPkg(*usePkgRm, *usePkgCategory)
+		if err != nil {
+			fmt.Println(err.Error())
+			os.Exit(1)
+		}
+		os.Exit(0)
+	}
+
+	if *useKittyFzfTab {
+		err := kitty.SwitchCurrentWindowTab()
 		if err != nil {
 			fmt.Println(err.Error())
 			os.Exit(1)
