09966bed86baabb37d885a4d996701274b77cd76
Dekr0 <dekr0.dk@protonmail.com>
2025-04-04 09:13:50 -0600

    First draft of config file. It only supports bookmarking of walker.

---
+++ config/lib.go
@@ -0,0 +1,124 @@
+package config
+
+import (
+	"encoding/json"
+	"os"
+	"path/filepath"
+	"slices"
+)
+
+type Config struct {
+	BookmarkRoots []string `json:"BookmarkRoots"`
+}
+
+func (c *Config) SaveConfig() error {
+	b, err := json.MarshalIndent(c, "", "    ")
+	if err != nil {
+		return err
+	}
+
+	configPath, _, err := getConfigPath()
+	if err != nil {
+		return err
+	}
+
+	_, err = os.Lstat(configPath)
+	if err != nil {
+		if !os.IsNotExist(err) {
+			return err
+		}
+
+		f, err := os.Create(configPath)
+		if err != nil {
+			return err
+		}
+
+		_, err = f.Write(b)
+		if err != nil {
+			return err
+		}
+
+		return f.Close()
+	}
+
+	return os.WriteFile(configPath, b, 0666)
+}
+
+func (c *Config) Sanitize() {
+	if len(c.BookmarkRoots) <= 0 {
+		return
+	}
+
+	c.BookmarkRoots = slices.DeleteFunc(
+		c.BookmarkRoots,
+		func(root string) bool {
+			expanded := os.ExpandEnv(root)
+			_, err := os.Lstat(expanded)
+			return err != nil
+		},
+	)
+}
+
+func LoadConfig() (*Config, error) {
+	configPath, home, err := getConfigPath()
+
+	_, err = os.Lstat(configPath)
+	if err != nil {
+		if os.IsNotExist(err) {
+			return initConfig(configPath, home)
+		}
+	}
+
+	b, err := os.ReadFile(configPath)
+	if err != nil {
+		return nil, err
+	}
+
+	var conf Config
+
+	err = json.Unmarshal(b, &conf)
+	if err != nil {
+		return nil, err
+	}
+
+	conf.Sanitize()
+
+	return &conf, nil
+}
+
+func getConfigPath() (string, string, error) {
+	home, err := os.UserHomeDir()
+	if err != nil {
+		return "", "", err
+	}
+
+	return filepath.Join(home, ".shutil.json"), home, nil 
+}
+
+func initConfig(configPath string, home string) (*Config, error) {
+	f, err := os.Create(configPath)
+	if err != nil {
+		return nil, err
+	}
+
+	conf := Config{
+		[]string{ home },
+	}
+
+	blob, err := json.MarshalIndent(conf, "", "    ")
+	if err != nil {
+		return nil, err
+	}
+
+	_, err = f.Write(blob)
+	if err != nil {
+		return nil, err
+	}
+
+	err = f.Close()
+	if err != nil {
+		return nil, err
+	}
+
+	return &conf, nil
+}
